b08f415f33880c35b22199afc7c369df6272a152
Kresimir Sutalo <kresimir095@gmail.com>
2019-07-18 02:26:13 +0200

    :(

---
+++ dealer_router/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dealer_router
+{
+    public class Message
+    {
+        public string Nick { get; set; }
+        public string MessageContent { get; set; }
+
+        public Message(string nick,string message)
+        {
+            this.Nick = nick;
+            this.MessageContent = message;
+        }
+
+        public override string ToString()
+        {
+            return this.Nick + "," + this.MessageContent;
+        }
+    }
+}
---
+++ dealer_router/Program.cs
@@ -0,0 +1,38 @@
+﻿using dealer_router;
+using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Text;
+using ZeroMQ;
+
+namespace ZMQGuide
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var server = new ResponseSocket("@tcp://localhost:5556")) 
+            using (var client = new RequestSocket(">tcp://localhost:5556"))  
+            {
+                Console.WriteLine("Unesite nick");
+
+                var nick = Console.ReadLine();
+                Console.WriteLine("Unesite poruku");
+                var messageContent = Console.ReadLine();
+                Message message = new Message(nick, messageContent);
+                client.SendFrame(message.ToString());
+
+                string m1 = server.ReceiveFrameString();
+                string [] nickname = m1.Split(',');
+                Console.WriteLine("From Client: {0}", nickname[0]);
+
+                server.SendFrame("Hi "+nickname[0]);
+
+                string m2 = client.ReceiveFrameString();
+                Console.WriteLine("From Server: {0}", m2);
+
+                Console.Read();
+            }
+        }
+    }
+}
